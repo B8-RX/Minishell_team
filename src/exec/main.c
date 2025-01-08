cde155932db7bd7d8e637779fa12926c0aca2aad
Sofiane Sghiouar <sofiane.sghiouar@gmail.com>
2025-01-08 06:12:09 +0100

    feat(hooks): add temporary main to test valgrind hook and add branch main on valgrind targets

---
+++ src/exec/main.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssghioua <ssghioua@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 06:08:30 by ssghioua          #+#    #+#             */
+/*   Updated: 2025/01/08 06:08:32 by ssghioua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int main(void)
+{
+    printf("Hello, ceci est un main temporaire! Le main.c final est en construction...\n");
+    return 0;
+}
